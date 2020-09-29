955a30da2875f4b7393df4af4b7ff41ff716e1b5
simplexion-blechschmidt <blechschmidt@simplexion.de>
2020-09-29 12:15:45 +0200

    made template for csv file; added comments to code

--- pos2dpv.py
+++ pos2dpv.py
@@ -18,7 +18,7 @@ def parse_arguments():
     verbose.add_argument("-v", "--verbose", action="count",      help="set verbose loglevel")
 
     parser.add_argument('-f', '--feeder_setup_file', type=str, required=True, help='Feeder definition csv file.')
-    parser.add_argument('-p', '--pos_file',          type=str, required=True, help='KiCas POS file.')
+    parser.add_argument('-p', '--pos_file',          type=str, required=True, help='KiCad POS file.')
     parser.add_argument('-d', '--dpv_file',          type=str, required=True, help='PnP file.')
 
     args = parser.parse_args()
@@ -32,47 +32,55 @@ def configure_logging(verbose, quiet):
     logConfig(format="%(asctime)-15s %(levelname)-8s %(message)s", datefmt="%Y-%m-%d %H:%M:%S", level=log_level)
     debug("DEBUG Log Level")
 
-def auto_type(value):
-    result = value
-    if '' == value:
-        result = None
-    if value.upper() in ('Y', 'T') :
-        result = True
-    elif value.upper() in ('N', 'F'):
-        result = False
-    elif value.isnumeric():
-        result = int(value, 0)
-    else:
-        try:
-            result = float(value)
-        except ValueError:
-            pass
-    return result
-
 def csv_to_dict_list(csv_file_path):
+    # function to automatically turn strings into python values
+    def auto_type(value):
+        result = value
+        if '' == value:
+            result = None
+        if value.upper() in ('Y', 'T') :
+            result = True
+        elif value.upper() in ('N', 'F'):
+            result = False
+        elif value.isnumeric():
+            result = int(value, 0)
+        else:
+            try:
+                result = float(value)
+            except ValueError:
+                pass
+        return result
+    # read csv file and turn into list of python dicts
     with open(csv_file_path, 'r') as csv_file:
         return [{k: auto_type(v) for k, v in row.items()} for row in csv.DictReader(csv_file)]
 
 def find_feeder(feeders, component):
+    result = None
+    # concact value and package to component name
     component_identifiers = (component["Val"], component["Package"])
     component_name = "-".join((str(identifier) for identifier in component_identifiers if identifier))
+    # try to find matching feeder
     for feeder in feeders:
-        feeder_names = [feeder["Component"]]
+        # concat component and aliases of feeder
+        feeder_component_names = [feeder["Component"]]
         if feeder["Aliases"]:
-            feeder_names += feeder["Aliases"].split(":")
-        if component_name in feeder_names:
-            return feeder
-    return None
+            feeder_component_names += feeder["Aliases"].split(":")
+        # match
+        if component_name in feeder_component_names:
+            result = feeder
+    return result
 
 def main(args):
     now = datetime.now()
-    # read in files
-    components = csv_to_dict_list(args.pos_file)
-    feeders = [feeder for feeder in csv_to_dict_list(args.feeder_setup_file) if feeder["Component"]]
-    # assign each component to a feeder
     fiducials = list()
     placements = list()
     unplaced = list()
+    # read in files
+    components = csv_to_dict_list(args.pos_file)
+    feeders = csv_to_dict_list(args.feeder_setup_file)
+    # filter out unused feeders
+    feeders = [feeder for feeder in feeders if feeder["Component"]]
+    # assign each component to a feeder in a placement
     for component in components:
         if component["Ref"].startswith("FID"):
             fiducials += [component]
@@ -94,71 +102,71 @@ def main(args):
     for fiducial in fiducials:
         info("Fiducial {} at ({}, {})".format(fiducial["Ref"], fiducial["PosX"], fiducial["PosY"]))
     warning("Not placing {}".format(", ".join([component["Ref"] for component in unplaced])))
-    
+    # fill variables for jinja template
+    header = {
+        "file": path.basename(args.dpv_file),
+        "pcbfile": path.basename(args.pos_file),
+        "date": "{:02d}/{:02d}/{:02d}".format(now.year, now.month, now.day),
+        "time": "{:02d}:{:02d}:{:02d}".format(now.hour, now.minute, now.second),
+        "paneltype": 0,
+    }
+
+    stations = [
+        {
+            "id": feeder["Feeder Index"],
+            "delt_x": feeder["XOffset"],
+            "delt_y": feeder["YOffset"],
+            "feed_rates": feeder["Feed Spacing"],
+            "note": feeder["Component"],
+            "height": feeder["Height"],
+            "speed": feeder["Speed"],
+            "status": sum(
+                [
+                    0b001 if not feeder["Place Component"] else 0,
+                    0b010 if feeder["Check Vacuum"] else 0,
+                    0b100 if feeder["Use Vision"] else 0,
+                ]
+            ),
+            "size_x": 0,
+            "size_y": 0,
+            "height_take": 0,
+            "delay_take": 0,
+        } for feeder in feeders
+    ]
+
+    ecomponents = [
+        {
+            "p_head": feeder["Head"],
+            "s_t_no": feeder["Feeder Index"],
+            "delt_x": component["PosX"],
+            "delt_y": component["PosY"],
+            "angle": component["Rot"],
+            "height": feeder["Height"],
+            "skip":sum(
+                [
+                    0b001 if not feeder["Place Component"] else 0,
+                    0b010 if feeder["Check Vacuum"] else 0,
+                    0b100 if feeder["Use Vision"] else 0,
+                ]
+            ),
+            "speed": feeder["Speed"],
+            "explain": component["Ref"],
+            "note": feeder["Component"],
+            "delay": 0,
+        } for feeder, component in [[placement["feeder"], placement["component"]] for placement in placements]
+    ]
+
+    calib_points = [
+        {
+            "offset_x": fiducial["PosX"],
+            "offset_y": fiducial["PosY"],
+            "note": fiducial["Ref"],
+        } for fiducial in fiducials
+    ]
+
     with open(args.dpv_file, 'w') as dpv:
-        header = {
-            "file": path.basename(dpv.name),
-            "pcbfile": path.basename(args.pos_file),
-            "date": "{:02d}/{:02d}/{:02d}".format(now.year, now.month, now.day),
-            "time": "{:02d}:{:02d}:{:02d}".format(now.hour, now.minute, now.second),
-            "paneltype": 0,
-        }
-
-        stations = [
-            {
-                "id": feeder["Feeder Index"],
-                "delt_x": feeder["XOffset"],
-                "delt_y": feeder["YOffset"],
-                "feed_rates": feeder["Feed Spacing"],
-                "note": feeder["Component"],
-                "height": feeder["Height"],
-                "speed": feeder["Speed"],
-                "status": sum(
-                    [
-                        0b001 if not feeder["Place Component"] else 0,
-                        0b010 if feeder["Check Vacuum"] else 0,
-                        0b100 if feeder["Use Vision"] else 0,
-                    ]
-                ),
-                "size_x": 0,
-                "size_y": 0,
-                "height_take": 0,
-                "delay_take": 0,
-            } for feeder in feeders
-        ]
-
-        ecomponents = [
-            {
-                "p_head": feeder["Head"],
-                "s_t_no": feeder["Feeder Index"],
-                "delt_x": component["PosX"],
-                "delt_y": component["PosY"],
-                "angle": component["Rot"],
-                "height": feeder["Height"],
-                "skip":sum(
-                    [
-                        0b001 if not feeder["Place Component"] else 0,
-                        0b010 if feeder["Check Vacuum"] else 0,
-                        0b100 if feeder["Use Vision"] else 0,
-                    ]
-                ),
-                "speed": feeder["Speed"],
-                "explain": component["Ref"],
-                "note": feeder["Component"],
-                "delay": 0,
-            } for feeder, component in [[placement["feeder"], placement["component"]] for placement in placements]
-        ]
-
-        calib_points = [
-            {
-                "offset_x": fiducial["PosX"],
-                "offset_y": fiducial["PosY"],
-            } for fiducial in fiducials
-        ]
-
         with open('template.dpv.j2', 'r') as f:
             template = Template(f.read())
-        
         dpv.write(template.render(header=header, stations=stations, ecomponents=ecomponents, calib_points=calib_points))
 
 if "__main__" == __name__:
